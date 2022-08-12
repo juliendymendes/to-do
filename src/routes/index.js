4822336d3ee447b1e51ad6a0d5c41b939e4e1df4
Juliendy Mendes <juliendy0708@gmail.com>
2022-08-12 15:33:15 -0400

    fix: fixed import bug

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../pages/HomeScreen';
 import EditTaskScreen from '../pages/EditTaskScreen';
 import AuthenticationScreen from '../pages/AuthenticationScreen';
-import RegisterScreen from '../pages/RegisterScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,7 +9,6 @@ export default function Routes() {
 	return (
 		<Stack.Navigator screenOptions={{headerTransparent: true }} initialRouteName='Authentication'>
 			<Stack.Screen options={{headerShown: false}} name='Authentication' component={AuthenticationScreen}/>
-			<Stack.Screen options={{title: ''}} name='Register' component={RegisterScreen}/>
 			<Stack.Screen options={{headerShown: false}} name='Home' component={HomeScreen} />
 			<Stack.Screen options={{title: ''}} name='EditTask' component={EditTaskScreen}/>
 		</Stack.Navigator>
