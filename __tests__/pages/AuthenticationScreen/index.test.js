c3b7e549ed85f440e96d9bcb954ee8f1c12fdef7
Juliendy Mendes <juliendy0708@gmail.com>
2022-08-24 21:04:37 -0400

    chore: validateInput test

---
+++ __tests__/pages/AuthenticationScreen/index.test.js
@@ -0,0 +1,12 @@
+
+
+describe("pages/AuthenticationScreen", () => {
+
+	describe("validateInput", () => {
+		it("deve retornar true se o input não for vazio nem undefined, caso contrário, retorna false", () => {
+
+		})
+	})
+	 
+	
+})
